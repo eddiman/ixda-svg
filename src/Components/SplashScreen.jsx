3c156d29b43c3239d51cae6e3e7da860918cb86f
Edvard Pires Bjørgen <edbjorgen@gmail.com>
2025-02-03 10:37:24 +0100

    add photography scrollable board

--- src/Components/SplashScreen.jsx
+++ src/Components/SplashScreen.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react"
-import {Html } from "@react-three/drei"
 
 
 const SplashScreen = ({selected, color, ...props}) => {
--- src/Containers/Photography.jsx
+++ src/Containers/Photography.jsx
@@ -1,45 +1,72 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import * as THREE from "three";
 import SplashScreen from "../Components/SplashScreen";
+import PhotoGridItem from "../Components/PhotoGridItem";
 import NavBar from "../Components/NavBar";
-import "../styles/photography.scss"
-
-const rfs = THREE.MathUtils.randFloatSpread;
+import DraggableScrollContainer from "../Components/DraggableScrollContainer";
+import "../styles/photography.scss";
+import { useDraggable } from "react-use-draggable-scroll";
 
 const GRID_ITEMS = 100;
 const PASTEL_COLORS = [
-  'pink', 'purple', 'indigo', 'blue', 'cyan',
-  'teal', 'green', 'lime', 'yellow', 'orange'
+  "pink",
+  "purple",
+  "indigo",
+  "blue",
+  "cyan",
+  "teal",
+  "green",
+  "lime",
+  "yellow",
+  "orange",
 ];
-
-function PhotoGrid() {
-  const getColor = (index) => PASTEL_COLORS[index % PASTEL_COLORS.length];
-
+const PhotoGrid = ({ isLandscape }) => {
+  const [selectedIndex, setSelectedIndex] = useState(null);
   return (
-    <main className="photo-grid-main-container">
-          {Array.from({ length: GRID_ITEMS }).map((_, index) => (
-            <div
+    <DraggableScrollContainer enableDragScroll={true}>
+      <main className="photo-grid-main-container">
+        {Array.from({ length: GRID_ITEMS }).map((_, index) => {
+const isLandscape = Math.random() < 1
+          return (
+            <PhotoGridItem
               key={index}
-              className={`photo-grid-item`}
-              style={ {backgroundColor : getColor(index)} }
+              index={index}
+              isLandscape={isLandscape}
+              isSelected={selectedIndex === index}
+              selectedIndex={selectedIndex}
+              onSelect={() =>
+                setSelectedIndex(index === selectedIndex ? null : index)
+              }
             />
-          ))}
-    </main>
+          );
+        })}
+      </main>
+    </DraggableScrollContainer>
   );
 }
 
-
+//item.style.boxShadow = `${-rotateY * 5}px ${rotateX * 5}px 40px rgba(40, 124, 70, .2)`;
 
 export const Photography = () => {
   const [showLoading, setShowLoading] = useState(true);
-  
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setShowLoading(false);
-    }, 300); // 5000 milliseconds = 5 seconds
+    }, 300); // 300 milliseconds
     return () => clearTimeout(timeout);
   }, []); // Run effect only once when component mounts
-  
+
+  useEffect(() => {
+    // Center the user in the middle of the photo grid
+    const container = document.querySelector(".draggable-scroll-container");
+    if (container) {
+      container.scrollTo({
+        top: container.scrollHeight / 2 - container.clientHeight / 2,
+        left: container.scrollWidth / 2 - container.clientWidth / 2,
+      });
+    }
+  }, [showLoading]);
   return (
     <>
       {showLoading ? (
@@ -47,7 +74,7 @@ export const Photography = () => {
       ) : (
         <>
           <NavBar />
-          <PhotoGrid/>
+          <PhotoGrid isLandscape={Math.random() < 0.3} />
         </>
       )}
     </>
